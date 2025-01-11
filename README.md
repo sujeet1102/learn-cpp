fab5313bd43b8be6ec06e06da61a5136a0f1989c
sujeet1102 <sujeetrj1102@gmail.com>
2025-01-11 23:06:34 +0530

    Added getting-started project along with cmake files.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # learn-cpp
 Personal Quest of learning C++
+
+TO DO: 
+1. cmake easier compilation and execution 
+2. documentation for cmake
+3. add comments during cmake
---
+++ getting-started/src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(void)
+{
+    int fav_num;
+    std::cout << "Enter your favourite number: ";
+    std::cin >> fav_num;
+    std::cout << "Same here" << std::endl;
+    return 0;
+}
