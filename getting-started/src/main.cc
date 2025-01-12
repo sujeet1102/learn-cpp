c915edcdae40bf9279c538317a6a28e4f66adf0e
sujeet1102 <sujeetrj1102@gmail.com>
2025-01-12 14:59:49 +0530

    Added some cpp style variable initializations.

--- getting-started/src/main.cc
+++ getting-started/src/main.cc
@@ -5,6 +5,18 @@ int main(void)
     int fav_num;
     std::cout << "Enter your favourite number: ";
     std::cin >> fav_num;
-    std::cout << "Same here" << std::endl;
+    std::cout << "Same here: " << fav_num << std::endl;
+
+    // Initializations in cpp
+    // c style
+    int age = 16;
+    std::cout << age << std::endl;
+    // cpp constructor style
+    int month (12);
+    std::cout << month << std::endl;
+    // cpp list style c++11 standard
+    int year {1996};
+    std::cout << year << std::endl;
+    
     return 0;
 }
